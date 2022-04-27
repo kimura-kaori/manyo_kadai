6dcdcffc2d614a8b0590437f465722eaeed3c75c
kaori kimura <tkhy_1125@icloud.com>
2022-04-27 18:29:36 +0900

    一覧画面と新規作成機能を作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to tasks_path, notice:'作成しました'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:title, :content, :deadline, :priority, :status)
+  end
+end
