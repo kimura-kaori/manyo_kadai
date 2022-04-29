796b6d60ada691a5b63fc38b00baacb4b77b18b9
kaori kimura <tkhy_1125@icloud.com>
2022-04-29 14:57:29 +0900

    詳細画面へ遷移できない

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  # before do
-  #   FactoryBot.create(:task)
-  # end
+  before do
+    FactoryBot.create(:task)
+  end
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -30,8 +30,8 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-         # visit task_path(@task.id)
-         # expect(page).to have_content '詳細'
+         visit task_path(@task.id)
+         expect(page).to have_content '詳細'
        end
      end
   end
