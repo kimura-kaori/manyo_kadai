ab0f84b4dee82a07ac4770e257e4e7cac0358cce
kaori kimura <tkhy_1125@icloud.com>
2022-04-29 16:19:09 +0900

    詳細表示機能のSystem Specコード修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
+
 RSpec.describe 'タスク管理機能', type: :system do
-  before do
-    FactoryBot.create(:task)
-  end
+  let!(:task) { FactoryBot.create(:task, title: 'task', content: '卒業発表会', deadline: '2022-04-28', priority: '高', status: '未着手') }
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -30,8 +29,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-         visit task_path(@task.id)
-         expect(page).to have_content '詳細'
+         visit task_path(task.id)
+         binding.irb
+         expect(page).to have_content '卒業発表会'
        end
      end
   end
