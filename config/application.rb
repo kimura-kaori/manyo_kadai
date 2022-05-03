24d6051cf210cc6b7bb0abf45868a4a312ff6cb4
kaori kimura <tkhy_1125@icloud.com>
2022-05-03 16:47:58 +0900

    ステータスをプルダウンで入力できるように設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ManyoKadai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.test_framework :rspec,
                    model_specs: true,
