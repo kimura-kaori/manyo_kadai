f0a68bbc84e0665e38557ba2cea0d4b2a883fe8d
kaori kimura <tkhy_1125@icloud.com>
2022-05-06 14:44:41 +0900

    ステータス検索をした場合のsystemspecを追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -32,4 +32,27 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task1'
       end
     end
+
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        visit tasks_path
+        select 'start', from: 'status'
+        click_on "検索"
+        expect(page).to have_content 'Factoryで作ったタイトル1'
+        expect(page).to have_content 'task1'
+        expect(page).to have_content '最後のタスク'
+      end
+    end
+
+    # context 'タイトルのあいまい検索とステータス検索をした場合' do
+    #   it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+    #     visit tasks_path
+    #     fill_in 'title', with: "メーカー直送"
+    #     select 'start', from: 'status'
+    #     click_on "検索"
+    #     expect(page).to have_content 'Factoryで作ったタイトル1'
+    #     expect(page).to have_content 'task1'
+    #     expect(page).to have_content '最後のタスク'
+    #   end
+    # end
 end
