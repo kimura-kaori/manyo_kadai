ec1539701ce99a0898f8e11cc0ef47dfeb57a367
kaori kimura <tkhy_1125@icloud.com>
2022-05-07 10:51:21 +0900

    modelspecを記述

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,18 +1,10 @@
 FactoryBot.define do
-  # factory :task do
-  #   title { 'test_title' }
-  #   content { 'test_content' }
-  #   deadline { 'test_deadline' }
-  #   priority { 'test_priority' }
-  #   status { 'test_status' }
-  # end
-  #
   #実際にテストの時に入力するデータ
   factory :task do
     title { 'Factoryで作ったタイトル1' }
     content { 'Factoryで作ったコンテント1' }
     deadline { '2022-03-03' }
-    priority { '低' }
+    priority { '高' }
     status { 'start' }
   end
 
@@ -20,7 +12,7 @@ FactoryBot.define do
     title { 'Factoryで作ったタイトル2' }
     content { 'Factoryで作ったコンテント2' }
     deadline { '2022-04-04' }
-    priority { '高' }
+    priority { '低' }
     status { 'start' }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,27 +1,31 @@
 require 'rails_helper'
-
-# RSpec.describe Task, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-
-  describe 'タスクモデル機能', type: :model do
-    describe 'バリデーションのテスト' do
-      context 'タスクのタイトルが空の場合' do
-        it 'バリデーションにひっかる' do
-          task = Task.new(title: '', content: '失敗テスト')
-          expect(task).not_to be_valid
-        end
+describe 'タスクモデル機能', type: :model do
+  describe '検索機能' do
+    let!(:task) { FactoryBot.create(:task, title: 'テスト', status: 'yet') }
+    let!(:second_task) { FactoryBot.create(:second_task, title: "サンプル", status: 'start') }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        expect(Task.get_by_title('テスト')).to include(task)
+        expect(Task.get_by_title('テスト')).not_to include(second_task)
+        expect(Task.get_by_title('テスト').count).to eq 1
       end
-      context 'タスクの詳細が空の場合' do
-        it 'バリデーションにひっかかる' do
-          task = Task.new(title: '失敗テスト', content: '')
-          expect(task).not_to be_valid
-        end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.get_by_status('yet')).to include(task)
+        expect(Task.get_by_status('yet')).not_to include(second_task)
+        expect(Task.get_by_status('yet').count).to eq 1
       end
-      context 'タスクのタイトルと詳細に内容が記載されている場合' do
-        it 'バリデーションが通る' do
-          task = Task.new(title: 'task', content: '卒業発表会')
-          expect(task).not_to be_valid
-        end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        expect(Task.get_by_title('テスト')).to include(task)
+        expect(Task.get_by_status('yet')).to include(task)
+        expect(Task.get_by_title('テスト')).not_to include(second_task)
+        expect(Task.get_by_status('yet')).not_to include(second_task)
+        expect(Task.get_by_title('テスト').count).to eq 1
+        expect(Task.get_by_status('yet').count).to eq 1
       end
     end
   end
+end
