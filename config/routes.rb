5a4be088a3e0ee5a157f7350b645e2ff8ea63210
kaori kimura <tkhy_1125@icloud.com>
2022-05-09 11:50:06 +0900

    ログインフォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'search' => 'tasks#search'
   resources :tasks
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only:[:new, :create, :destroy]
 end
