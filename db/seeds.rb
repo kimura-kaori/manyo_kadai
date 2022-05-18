a78726fd1450587289e24b25dfc2636def670040
kaori kimura <tkhy_1125@icloud.com>
2022-05-18 18:43:06 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# 3.times do |n|
-#   name = Faker::Name.name
-#   email = Faker::Internet.email
-#   password = "password"
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                password_confirmation: password,
-#                admin: false,
-#                )
-# end
-# User.create!(name: "管理者",
-#              email: "admin@admin.com",
-#              password: "111111",
-#              password_confirmation: "111111",
-#              admin: true)
+10.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               admin: false,
+               )
+end
+User.create!(name: "管理者",
+             email: "admin@admin.com",
+             password: "111111",
+             password_confirmation: "111111",
+             admin: true)
 
-3.times do |i|
+10.times do |i|
   Label.create!(name: "sample#{i + 1}")
+end
+
+user_id = User.all[0..10]
+label_ids = Label.all[0..10]
+
+10.times do |i|
+  Task.create!(
+    title: "test#{i + 1}",
+    content: "テスト",
+    deadline: "2022-05-05",
+    priority: rand(0..2),
+    status: rand(0..2),
+    user_id: rand(10),
+    label_ids: rand(10)
+
+)
 end
